fd039b9b03a657c391d2e0716bb061a08c6942bd
hae <haedesu902@gmail.com>
2019-05-10 14:14:39 +0900

    色々修正

--- src/main/java/com/github/hae902/variousmessages/Main.java
+++ src/main/java/com/github/hae902/variousmessages/Main.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,19 +25,39 @@ public class Main extends JavaPlugin implements Listener{
 		String name = player.getDisplayName();
 		if (!player.hasPlayedBefore()) {
 			player.teleport(new Location(player.getWorld(), 179.5, 64, 79.5));
-			event.setJoinMessage(ChatColor.YELLOW + name + "が 初めてサーバーにログインしました");
+			Bukkit.broadcastMessage(ChatColor.YELLOW + name + "が なかまになりたそうに こちらを見ている！");
+			event.setJoinMessage("なかまにしますか？ → はい");
+
 			player.getWorld().playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1, 1);
 		}else {
-			event.setJoinMessage(ChatColor.YELLOW + name + "が ログインしました。");
+			event.setJoinMessage(ChatColor.YELLOW + name + "が あらわれた！");
 			player.getWorld().playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_BELL, 1, 1.1f);
 
 		}
 	}
+	int playerInBed = 0;
 	@EventHandler
 	public void bed(PlayerBedEnterEvent event) {
 		Player player = event.getPlayer();
 		String name = player.getDisplayName();
 		if (event.getBedEnterResult() != BedEnterResult.OK) return;
-		Bukkit.broadcastMessage(name + "が 寝ました。");
+		playerInBed++;
+		if (Bukkit.getOnlinePlayers().size() != 1) {
+			Bukkit.broadcastMessage(ChatColor.YELLOW + name + "が 寝ました。");
+		}
+
+		if (Bukkit.getOnlinePlayers().size() / 2 <= playerInBed) {
+			Bukkit.broadcastMessage(ChatColor.GOLD + "朝になりました。");
+			player.getWorld().setTime(1000);
+			player.getWorld().setStorm(false);
+			player.getWorld().setThundering(false);
+		}else {
+			Bukkit.broadcastMessage(ChatColor.YELLOW + "あと" + ((Bukkit.getOnlinePlayers().size() / 2) - playerInBed) + "人寝ると朝になります。");
+		}
+	}
+
+	@EventHandler
+	public void BedLeave(PlayerBedLeaveEvent event) {
+		playerInBed--;
 	}
 }
